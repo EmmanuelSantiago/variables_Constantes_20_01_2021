addcd94c11930b09815991745aaf7d82fc6d9c15
Emmanuel Santiago <biosinx@gmail.com>
2021-01-20 21:00:31 -0500

    variables_constantes_20_01_2021

---
+++ main.js
@@ -0,0 +1,18 @@
+let vaso = "agua";
+let cantidad = 5;
+let fria = true;
+var lista_usuarios = ["Emmanuel", "Santiago", "Forero"];
+var informacion_adicional = {
+    mesa: 5,
+    silla: "frontal",
+    lista_plato : ["Redondo", "Cuadrado", "Rectangular"]
+};
+
+const formula = "H2O";
+
+console.log(formula);
+console.log(vaso);
+console.log(cantidad);
+console.log(fria);
+console.log(lista_usuarios);
+console.log(informacion_adicional)
